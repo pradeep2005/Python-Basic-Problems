d17e775a31e154c690add772a893624998fc12ae
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 22:48:14 +0530

    tuple_fruits.py

---
+++ tuple_fruits.py
@@ -0,0 +1,7 @@
+#Check whether "Banana" exists in the tuple.
+fruit=input("Enter a fruit name to check: ")
+fruits = ("apple","banana","orange")
+if fruit in fruits:
+    print("fruit Found")
+else:
+    print("fruit not found")
