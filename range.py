c748288642c423f3d446c08654aab3689e0cffc9
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 23:20:36 +0530

    range.py

---
+++ range.py
@@ -0,0 +1,13 @@
+#1
+for i in range(10,0,-1):
+    print(i)
+#2
+for i in range(1, 11):
+    print(f"5 x {i} = {5 * i}")
+#3
+sum = 0
+
+for i in range(1, 11):
+    sum = sum + i
+
+print("Sum =", sum)
