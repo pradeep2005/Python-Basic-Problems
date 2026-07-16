e9c31053b546b04d8ddafa0f26d1b366f6a55bd4
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 23:28:22 +0530

    Functions.py

---
+++ Functions.py
@@ -0,0 +1,21 @@
+#1
+def hello():
+    print("Hello,Python!")
+hello()
+hello()
+hello()
+#2
+def square():
+    a=5
+    print(a*a)
+square()
+#3
+def student():
+    name="pradeep"
+    depatment="AI&DS"
+    college="Ramco Institute of Technology"
+    print("Name:",name)
+    print("Department:",depatment)
+    print("College:",college)
+student()
+
