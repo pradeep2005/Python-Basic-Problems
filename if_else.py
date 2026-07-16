317d200cefd8a45b01ca828f5b9fb44455a8b797
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 23:11:17 +0530

    if_else.py

---
+++ if_else.py
@@ -0,0 +1,20 @@
+#1
+whether=int(input("enter numer:"))
+if whether>=0:
+    print("Positive")
+else:
+    print("Negative")
+
+#2
+whether_age=int(input("enter numer:"))
+if whether_age >=18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+#3
+num=int(input("Enter a number:"))
+if num %2==0:
+    print("Even Number")
+else:
+    print("Odd Number")
