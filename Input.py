6ba48ab0cef6c1703e0f48394e93a768b25e02ae
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 22:56:53 +0530

    Input.py

---
+++ Input.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+department=input("Enter you department:")
+age=int(input("Enter your age:"))
+
+print("Name:",name)
+print("Department:",department)
+print("Age:",age)
+
+age=input("enter age:")
+print(age)
+
+age1=int(input("enter age:"))
+print(age)
+
