e78dbcb1623444bbb35139b0cd04e7a7e6c900ba
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 22:56:07 +0530

    data_types.py

---
+++ data_types.py
@@ -0,0 +1,13 @@
+job="python"
+mark=100
+cgpa=99.9
+placed=True
+print("job:",job)
+print("mark:",mark)
+print("cgpa:",cgpa)
+print("placed:",placed)
+
+print(type(job))
+print(type(mark))
+print(type(cgpa))
+print(type(placed))
