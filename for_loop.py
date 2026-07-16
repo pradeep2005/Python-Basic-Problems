3166c63d82b77da10bf59d5cb68275dfca338476
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 23:18:58 +0530

    for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+#1
+a=int(input("enter the number:"))
+
+for i in range(a):
+    print(i)
+
+#2
+name = input("Enter your name: ")
+
+for i in range(5):
+    print(name)
+#3
+even=int(input("enter the number:"))
+for i in range(even):
+    if i % 2==0:
+        print(i)
+
