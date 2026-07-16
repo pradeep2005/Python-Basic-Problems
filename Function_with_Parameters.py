b14bbc2643211507168ba06ac49846cb85d721b3
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-16 23:29:51 +0530

    Function_with_Parameters.py

---
+++ Function_with_Parameters.py
@@ -0,0 +1,19 @@
+#1
+def square(num):
+    print("num:",num*num)
+square(8)
+#2
+def largest(a,b):
+    if a > b :
+        print(a,"is greader then")
+    else:
+        print(b,"is greader then")
+
+largest(15,20)
+
+#3
+def employee(name,salary):
+    print("name:",name)
+    print("salary:", salary)
+employee("pradeep",35000)
+
