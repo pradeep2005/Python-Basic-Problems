0276983365f4cf2479afb6625eb51516209b7652
Pradeep2005 <pradeepgovindaraj.cs@gmail.com>
2026-07-17 17:32:44 +0530

    set.py

---
+++ set.py
@@ -0,0 +1,26 @@
+numbers={1, 2, 3, 4}
+print(numbers)
+
+numbers.add(5)
+print(numbers)
+
+numbers.remove(2)
+print(numbers)
+
+#for loop
+for num in numbers:
+    print(num)
+
+#next problem
+
+colors={"Red","Green","blue","Red"}
+print(colors)
+
+colors.add("Yellow")
+print(colors)
+
+colors.remove("Green")
+print(colors)
+
+for num in colors:
+    print(num)
